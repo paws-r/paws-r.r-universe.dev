96872e350664f8fcafeec87fd51b46d5b876401d
DyfanJones <dyfanjones@hotmail.com>
2023-01-13 23:45:50 +0000

    update package order for sdk build

--- update_universe.R
+++ update_universe.R
@@ -5,4 +5,27 @@ df <- data.frame(package = list.files(paws_tree))
 df$url <- url
 df$subdir <- sprintf("cran/%s", df$package)
 paws.common <- c("paws.common", url, "paws.common")
-jsonlite::write_json(rbind(df, paws.common), "packages.json", pretty = T)
+df <- rbind(df, paws.common)
+
+list_sub_cat_pkgs <- function(pkgs){
+  return(pkgs[grepl(".p[0-9]+$", pkgs)])
+}
+
+list_cat_pkgs <- function(pkgs){
+  pkgs <- pkgs[grepl("paws[.].*$", pkgs)]
+  pkgs <- pkgs[!grepl(".p[0-9]+$", pkgs)]
+  return(pkgs[!grepl("paws.common", pkgs)])
+}
+
+build_order <- c(
+  "paws.common",
+  list_sub_cat_pkgs(df$package),
+  list_cat_pkgs(df$package),
+  "paws"
+)
+
+jsonlite::write_json(
+  df[match(build_order, df$package),],
+  "packages.json",
+  pretty = T
+)
